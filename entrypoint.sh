f82a0d269986410f42ea4bc661583ac788afaf64
Senio Caires <seniocaires@gmail.com>
2017-11-17 09:40:58 -0200

    condicional envs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-sh -c "sed -i.old -e 's/api_key.*$/api_key = ${API_KEY}/' /etc/amplify-agent/agent.conf"
+
+if [ -n "${API_KEY}" ]; then
+    sh -c "sed -i.old -e 's/api_key.*$/api_key = ${API_KEY}/' /etc/amplify-agent/agent.conf"
+fi
+
 sh -c "sed -i.old -e 's/hostname.*$/hostname = ${HOSTNAME}/' /etc/amplify-agent/agent.conf"
 sh -c "sed -i.old -e 's/uuid.*$/uuid = ${HOSTNAME}/' /etc/amplify-agent/agent.conf"
-sh -c "sed -i.old -e 's/imagename.*$/imagename = ${IMAGE_NAME}/' /etc/amplify-agent/agent.conf"
+
+if [ -n "${IMAGE_NAME}" ]; then
+    sh -c "sed -i.old -e 's/imagename.*$/imagename = ${IMAGE_NAME}/' /etc/amplify-agent/agent.conf"
+fi
+
 service amplify-agent start
 nginx -g 'daemon off;'
